8b4a091ce4bfbdcc22d2694bac98a44c08f8a413
Vignesh Balaji <bv1494@gmail.com>
2025-08-09 21:45:50 -0700

    fix: remove nested repo and add Xcode project files

---
+++ VoiceMap/VoiceMap/VoiceMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMapApp.swift
+//  VoiceMap
+//
+//  Created by Vignesh Balaji on 8/9/25.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoiceMap/VoiceMapTests/VoiceMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMapTests.swift
+//  VoiceMapTests
+//
+//  Created by Vignesh Balaji on 8/9/25.
+//
+
+import Testing
+@testable import VoiceMap
+
+struct VoiceMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
